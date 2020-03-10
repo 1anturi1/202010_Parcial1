ce2e91b2913f38dba6e3c0cc9886219f7b254b31
1anturi1 <ai.anturi@uniandes.edu.co>
2020-03-09 23:18:59 -0500

    finished

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,48 +1,76 @@
 let socket = io.connect("http://localhost:3000", { "forceNew": true });
 
-socket.on("messages", data => {
+let mostar = false;
+let termine = false;
+let empresa = "";
+
+
+if (!mostar) {
+    document.getElementById('btnofertar').disabled = true;
+}
+
+function registrarse() {
+    deshabForm();
+    empresa = document.getElementById('razonsocial').value;
+    mostar = true;
+    habilOfertar();
+}
+
+function habilOfertar() {  
+    document.getElementById('btnofertar').disabled = false;
+}
+
+function ofertar() {
+
+    addPartici(empresa)
+    document.getElementById('btnofertar').disabled = true;
+    setTimeout(() => {
+        if (!termine) {
+            document.getElementById('btnofertar').disabled = false;
+        }
+    }, 30000);
+}
+
+socket.on("ofertas", data => {
     console.log(data);
     render(data);
 });
 
 function render(data) {
-    let html = data.map((e, i) => {
+    let html = data.map((e, i) => {if (e.valor.includes("Oferta aceptada")) {
+        document.getElementById('btnofertar').disabled = true;
+        termine = true;
         return (`
         <div>
-            <strong> ${e.author}</strong>
-            <em>${e.text}</em>
+            <strong>${e.razonsocial}</strong>
+            <strong>${e.valor}</strong>
         </div>
-        `);
-    }).join("");
-    
-    document.getElementById("messages").innerHTML = html;
+    `);
+    }
+    else{
+        return (`
+        <div>
+            <strong>${e.razonsocial}</strong>
+            <em>${e.valor}</em>
+        </div>
+    `);
+    }
+}).join("");
+    document.getElementById("campofertas").innerHTML = html;
 }
 
 
 
-function addPartici() {
-    let message = {
-        author: document.getElementById("nit").value,
-        text: document.getElementById("razonsocial").value
-    };
-    console.log("Emitting new message");
-    socket.emit("new-message", message);
-    document.getElementById("nit").value = "";
-    document.getElementById("razonsocial").value = "";
-
+function addPartici(empresa) {    
+    console.log("Emmiting new message");
+    socket.emit("new-message", empresa);
     return false;
 }
 
-function addMessage() {
-    let message = {
-        author: document.getElementById("username").value,
-        text: document.getElementById("text").value
-    };
-    console.log("Emitting new message");
-    socket.emit("new-message", message);
-    document.getElementById("username").value = "";
-    document.getElementById("text").value = "";
-
-    return false;
+function deshabForm() {
+    frm = document.forms['formulario'];
+    for (i = 0; ele = frm.elements[i]; i++)
+        ele.disabled = true;
 }
+
 
--- socketApi.js
+++ socketApi.js
@@ -2,34 +2,63 @@ let socket_io = require("socket.io");
 var io = socket_io();
 var socketApi = {};
 socketApi.io = io;
+let ofertas = [] ;
+let ultV = 0;
 
-var static = require('node-static');
-var http = require('http');
-var file = new(static.Server)();
-
-var app = http.createServer(function(req, res) {
-    file.serve(req, res);
-}).listen(8001);
-
-var io = require('socket.io').listen(app);
-
-var empresas = ''; 
 
 io.on('connection', function (socket) {
-    io.sockets.emit('empresa', empresas);
-
+    io.sockets.emit('ofertas', ofertas);
     socket.on("new-message", data => {
         socketApi.sendNotification(data)
     });
 });
 
 socketApi.sendNotification = data => {
-    empresas = data ;
-    io.sockets.emit('empresa', empresas);
+    let oferta = {
+        razonsocial: data,
+        valor: "",
+        num:0
+    }
+    let PB = Math.random()*(0.3 - 0.8) + 0.3
+    let PO = Math.random()*(0.3 - 0.8) + 0.3
+    let aceptada = ""
+    if(PO>PB)
+    { 
+        aceptada = "[Oferta aceptada"
+        if(ofertas.length ===0)
+        {
+            let base = 150000000 ;
+            oferta.num = base;
+            oferta.valor = aceptada + " valor $"+ Intl.NumberFormat("de-DE").format(base);
+            ultV = oferta.valor;
+        }
+    
+        else{
+            let aleatorio = (Math.random() * (10000000-5000000) + 5000000)
+            oferta.num = Math.trunc(ultV + aleatorio )    
+            oferta.valor = aceptada + " valor $"+Intl.NumberFormat("de-DE").format(Math.trunc(ultV + aleatorio ) +"]");
+        }
+    }
+    else{
+        aceptada = "[Oferta no aceptada]"
+        if(ofertas.length ===0)
+        {
+            oferta.num = 150000000;
+            oferta.valor = aceptada
+            ultV = oferta.num;
+        }
+        else{
+            let aleatorio = (Math.random()*(10000000-5000000) + 5000000)
+            oferta.num= Math.trunc(ultV + aleatorio )
+            oferta.valor = aceptada;
+        }
+    } 
+    
+    ofertas.push(oferta);
+    io.sockets.emit('ofertas', ofertas);
 }
 
-socketApi.getEmpresas = data => {
-    return empresas;
-};
+
+
 
 module.exports = socketApi;
